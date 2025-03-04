af9631097e8f64ba7320778c58be07557a8d3081
Thitikorn Nupan <thitikorn.n@g-able.com>
2025-03-04 21:32:39 +0700

    modified hello world endpoint and add lombok dependency

--- src/main/java/com/ttknp/testspringbootapp/TestSpringBootAppApplication.java
+++ src/main/java/com/ttknp/testspringbootapp/TestSpringBootAppApplication.java
@@ -1,5 +1,6 @@
 package com.ttknp.testspringbootapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,30 +8,31 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j // config logback by using annotation
 @SpringBootApplication
 @RestController
 public class TestSpringBootAppApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(TestSpringBootAppApplication.class);
+    // ** private static final Logger log = LoggerFactory.getLogger(TestSpringBootAppApplication.class);
 
     /**
-    @GetMapping
-    @ResponseBody
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public String hello() {
-        log.info("http://localhost:8080 is requested");
-        return "Hello World";
-    }*/
+     * @GetMapping
+     * @ResponseBody
+     * @ResponseStatus(HttpStatus.ACCEPTED) public String hello() {
+     * log.info("http://localhost:8080 is requested");
+     * return "Hello World";
+     * }
+     */
 
 
-    @GetMapping(value = {"/hello-world", "hello"})
+    @GetMapping(value = {"/hello-world", "/hello","/",""})
     public ResponseEntity hello() {
         log.info("http://localhost:8080/hello-world or /hello is requested");
-        return new ResponseEntity("Hello World", HttpStatus.ACCEPTED);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body("Hello World");
     }
 
 
