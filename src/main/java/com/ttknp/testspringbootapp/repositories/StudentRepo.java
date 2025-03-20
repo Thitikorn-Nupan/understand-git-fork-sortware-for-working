19324c7ece4730dda0007aaea0562c789dd07040
Thitikorn Nupan <thitikorn.n@g-able.com>
2025-03-20 16:54:50 +0700

    commit
    - add comment

--- src/main/java/com/ttknp/testspringbootapp/repositories/StudentRepo.java
+++ src/main/java/com/ttknp/testspringbootapp/repositories/StudentRepo.java
@@ -4,6 +4,6 @@ import com.ttknp.testspringbootapp.entities.Student;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+// @Repository // not working
 public interface StudentRepo extends CrudRepository<Student,Integer> {
 }
